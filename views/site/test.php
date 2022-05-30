ecc64befd46ffaf05656fcd6379e319a47403bae
parrot server <parrot@web-server.com>
2022-05-30 08:26:43 +0000

    First commit

---
+++ views/site/test.php
@@ -0,0 +1,19 @@
+<?php
+use yii\helpers\Html;
+
+$this->title = 'TEST';
+?>
+
+<div class="numberform">
+    <h1 class="display-4", style="text-align: center">Владик ЛОХ</h1>
+    <div class="jumbotron text-center bg-transparent">
+        <h1 class="display-4">Test</h1>
+
+    </div>
+
+    <div class="body-content">
+        
+
+    </div>
+</div>
+
