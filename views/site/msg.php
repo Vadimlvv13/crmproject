f2cd96b4367a9d5c3d9369ba678d11f1525e858c
Vadim Lisovyi <lisoviy13@gmail.com>
2022-05-31 17:10:37 +0300

    Edit view msg

--- views/site/msg.php
+++ views/site/msg.php
@@ -5,9 +5,9 @@ $this->title = 'msg';
 ?>
 
 <div class="numberform">
-    <h1 class="display-4", style="text-align: center">Владик ЛОХ</h1>
+    <h1 class="display-4", style="text-align: center">Новий проект</h1>
     <div class="jumbotron text-center bg-transparent">
-        <h1 class="display-4">Test</h1>
+       <!-- <h1 class="display-4">Test</h1>-->
 
     </div>
 
