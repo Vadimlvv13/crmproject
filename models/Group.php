04ab2ae32998c9717bafbabc02853941fdf080df
parrot server <parrot@web-server.com>
2022-06-03 12:48:50 +0000

    Create models/Group

---
+++ models/Group.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "group".
+ *
+ * @property int $id id
+ * @property string $name Назва групи
+ * @property int $id_manager id Керівника групи
+ */
+class Group extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'group';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'id_manager'], 'required'],
+            [['id_manager'], 'integer'],
+            [['name'], 'string', 'max' => 32],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'id_manager' => 'Id Manager',
+        ];
+    }
+}
