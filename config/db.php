223d45a4dfefada244a5bdf56d83a5f33d4410c2
Vadim Lisovyi <lisoviy13@gmail.com>
2022-06-03 15:11:50 +0300

    Edit config/db

--- config/db.php
+++ config/db.php
@@ -2,10 +2,10 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+    'dsn' => 'mysql:host=localhost;dbname=crmproject',
+    'username' => 'vladmin',
+    'password' => 'Regist-78',
+    'charset' => 'utf8mb4',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
