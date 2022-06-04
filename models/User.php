41f27f06f897162d85127c8bcfde705fba20936f
Vadim Lisovyi <lisoviy13@gmail.com>
2022-06-04 17:10:59 +0300

    Edit models/user

--- models/User.php
+++ models/User.php
@@ -3,6 +3,11 @@
 namespace app\models;
 
 use Yii;
+use yii\base\NotSupportedException;
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveRecord;
+use yii\web\IdentityInterface;
+use yii\filters\AccessControl;
 
 /**
  * This is the model class for table "user".
@@ -11,6 +16,9 @@ use Yii;
  * @property string $role Роль
  * @property string $login Логін
  * @property string $pass Пароль
+ * @property string $auth_key Ключ авторизації
+ * @property integer $creat_at Час створення
+ * @property integer $updat_at Час останнього оновлення
  * @property string $name ПІБ
  * @property string|null $age Дата народження
  * @property string|null $position Посада
@@ -20,9 +28,14 @@ use Yii;
  * @property float|null $zp_h ЗП на годину
  * @property int $id_group id групи
  * @property int $status Статус
+ * @property string $password write-only password
  */
-class User extends \yii\db\ActiveRecord
+class User extends ActiveRecord implements IdentityInterface
 {
+
+    const STATUS_DELETED = 0;
+    const STATUS_ACTIVE = 1;
+
     /**
      * {@inheritdoc}
      */
@@ -32,21 +45,114 @@ class User extends \yii\db\ActiveRecord
     }
 
     /**
+         * @inheritdoc
+         */
+        public function behaviors()
+        {
+            return [
+                TimestampBehavior::className(),
+            ];
+        }
+
+    /**
      * {@inheritdoc}
      */
     public function rules()
     {
         return [
-            [['role', 'login', 'pass', 'name', 'id_group', 'status'], 'required'],
-            [['age'], 'safe'],
-            [['type_zp', 'id_group', 'status'], 'integer'],
-            [['zp_h'], 'number'],
-            [['phone'], 'string', 'max' => 13],
-            [['role'], 'string', 'max' => 16],
-            [['login', 'pass', 'position', 'email'], 'string', 'max' => 32],
-            [['name'], 'string', 'max' => 64],
-            [['login'], 'unique'],
+            ['status', 'default', 'value' => self::STATUS_ACTIVE],
+            ['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_DELETED]],
+            [['role', 'login', 'pass', 'name', 'id_group', 'status'], required],
+            [['age'], safe],
+            [['type_zp', 'id_group', 'status'], integer],
+            [['zp_h'], number],
+            [['phone'], 'string', max => 13],
+            [['role'], 'string', max => 16],
+            [['login', 'pass', 'position', 'email'], string, max => 32],
+            [['name'], string, max => 64],
+            [['login'], unique],
+            [['email'], email],
         ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function findIdentity($id)
+    {
+        return static::findOne(['id' => $id, 'status' => self::STATUS_ACTIVE]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        throw new NotSupportedException('"findIdentityByAccessToken" is not implemented.');
+    }
+    
+    /**
+     * Finds user by login
+     *
+     * @param string $login
+     * @return static|null
+     */
+    public static function findByUsername($login)
+    {
+        return static::findOne(['login' => $login, 'status' => self::STATUS_ACTIVE]);
+    }
+    
+    /**
+     * @inheritdoc
+     */
+    public function getId()
+    {
+        return $this->getPrimaryKey();
+    }
+    
+    /**
+     * @inheritdoc
+     */
+    public function getAuthKey()
+    {
+        return $this->auth_key;
+    }
+    
+    /**
+     * @inheritdoc
+     */
+    public function validateAuthKey($authKey)
+    {
+        return $this->getAuthKey() === $authKey;
+    }
+    
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return bool if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return Yii::$app->security->validatePassword($password, $this->pass);
+    }
+    
+    /**
+     * Generates password hash from password and sets it to the model
+     *
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password_hash = Yii::$app->security->generatePasswordHash($password);
+    }
+    
+    /**
+     * Generates "remember me" authentication key
+     */
+    public function generateAuthKey()
+    {
+        $this->auth_key = Yii::$app->security->generateRandomString();
     }
 
     /**
@@ -56,18 +162,18 @@ class User extends \yii\db\ActiveRecord
     {
         return [
             'id' => 'ID',
-            'role' => 'Role',
-            'login' => 'Login',
-            'pass' => 'Pass',
-            'name' => 'Name',
-            'age' => 'Age',
-            'position' => 'Position',
-            'email' => 'Email',
-            'phone' => 'Phone',
-            'type_zp' => 'Type Zp',
-            'zp_h' => 'Zp H',
-            'id_group' => 'Id Group',
-            'status' => 'Status',
+            'role' => 'Доступ',
+            'login' => 'Логін',
+            //'pass' => 'Pass',
+            'name' => 'Повне ім\'я',
+            'age' => 'Дата народження',
+            'position' => 'Посада',
+            'email' => 'E-mail',
+            'phone' => 'Номер телефону',
+            'type_zp' => 'Тип заробітної плати',
+            'zp_h' => 'Заробітня плата на годину',
+            'id_group' => 'Група',
+            //'status' => 'Status',
         ];
     }
 }
