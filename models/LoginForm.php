d20ecf04104a376b88a741479a5b4d2ae531b7d6
Vadim Lisovyi <lisoviy13@gmail.com>
2022-06-05 23:41:42 +0300

    Edit LoginForm

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -13,8 +13,8 @@ use yii\base\Model;
  */
 class LoginForm extends Model
 {
-    public $username;
-    public $password;
+    public $login;
+    public $pass;
     public $rememberMe = true;
 
     private $_user = false;
@@ -27,11 +27,11 @@ class LoginForm extends Model
     {
         return [
             // username and password are both required
-            [['username', 'password'], 'required'],
+            [['login', 'pass'], 'required'],
             // rememberMe must be a boolean value
             ['rememberMe', 'boolean'],
             // password is validated by validatePassword()
-            ['password', 'validatePassword'],
+            ['pass', 'validatePassword'],
         ];
     }
 
@@ -47,7 +47,7 @@ class LoginForm extends Model
         if (!$this->hasErrors()) {
             $user = $this->getUser();
 
-            if (!$user || !$user->validatePassword($this->password)) {
+            if (!$user || !$user->validatePassword($this->pass)) {
                 $this->addError($attribute, 'Incorrect username or password.');
             }
         }
@@ -73,7 +73,7 @@ class LoginForm extends Model
     public function getUser()
     {
         if ($this->_user === false) {
-            $this->_user = User::findByUsername($this->username);
+            $this->_user = User::findByUsername($this->login);
         }
 
         return $this->_user;
