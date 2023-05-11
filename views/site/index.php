73d5fcbae9b9cdce78b0af192d70f5ec4a20110f
Vadim Lisovyi <lisoviy13@gmail.com>
2023-05-11 18:54:16 +0300

    Edit views/site/index

--- views/site/index.php
+++ views/site/index.php
@@ -8,9 +8,6 @@ $this->title = 'CRM Project';
 
     <div class="jumbotron text-center bg-transparent">
         <h1 class="display-4">Слава Україні!</h1>
-
-        <p class="lead">Русский военный корабль — иди нахуй!</p>
-
     </div>
 
     <div class="body-content">
